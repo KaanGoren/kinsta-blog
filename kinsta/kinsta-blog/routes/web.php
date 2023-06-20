58223bb7f22aa9a62856371ee768f1ad40cd6bd1
Selçuk Kaan Gören <TAVSANYT@gmail.com>
2023-06-20 14:39:41 +0200

    finish

---
+++ kinsta/kinsta-blog/routes/web.php
@@ -0,0 +1,20 @@
+<Link href="/logout" method="post" as="button" type="button">Logout</Link>
+<?php
+
+use App\Models\Article;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+
+Route::get('/', function () {
+    return Inertia::render('Index', [
+        'articles' => Article::latest()->get()
+    ]);
+})->name('home');
+
+Route::get('/posts/{article:id}', function (Article $article) {
+    return Inertia::render('Show', [
+        'article' => $article
+    ]);
+})->name('article.show');
